edcdcc4d5ce83db925477d0fb38bd52e460dc58c
devjhonluna <dev.jhonluna@gmail.com>
2025-09-20 15:12:32 -0500

    subida del esqueleto de app de clima con open meteor api para frontend mentor

---
+++ src/components/dropdown/index.js
@@ -0,0 +1,3 @@
+export {default as Dropdown} from "./Dropdown.astro"
+export {default as DropdownContent} from "./DropdownContent.astro"
+export {default as DropdownTrigger} from "./DropdownTrigger.astro"
